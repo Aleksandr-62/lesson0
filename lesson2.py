905bf2124800c2f584c6f739791d6a6afee39005
Александр <shurik-zet@mail.ru>
2024-08-07 14:37:10 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+ #Урок 2 "Переменные"
+Number_of_completed_DZ = 12 #кол-во выполненных ДЗ
+number_of_hours_spent = 1.5 #кол-во затраченных часов
+Course_name = 'Python' #название курса
+print("Курс",str(Course_name),", ",
+      "всего задач:",str(Number_of_completed_DZ),",",
+      "затрачено часов:",str(number_of_hours_spent),",",
+      "среднее время выполнения ",float(number_of_hours_spent)/int(Number_of_completed_DZ))
