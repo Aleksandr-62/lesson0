a19e423b1044d70e7cdbacdb0f1ce9233d24cc49
Александр <shurik-zet@mail.ru>
2024-12-14 07:46:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(12+89-1)
+#1nd programm
+print(9**0.5*5)
+
+#2nd programm
+print((99.9 > 99.8 and (100 != 100.1)))
+
+#3nd programm
+print(2*2+))
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#4nd programm
+print('123.456')
+print(float('123.456'))
+print(float('123.456')*10)
+print(float('123.456')*10%10)
+print(int(float('123.456')*10%10))
